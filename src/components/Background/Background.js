328acf9922e7fd59e79ede0019b64a1ce28373e5
ivansinabdeev <75542138+ivansinabdeev@users.noreply.github.com>
2021-12-26 17:46:54 +0100

    npm and any code

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,5 +1,14 @@
 import s from './Background.module.css';
 
 export default function Background({ children }) {
-  return <div className={s.background}>{children}</div>;
+  return (
+    <div className={s.background}>
+      {children}
+      <div></div>
+    </div>
+  );
 }
+
+//  className={activeValue === 'Транспорт' ? s.activeSvg : s.svg}
+
+// {isLoggedIn ? <UserMenu /> : null}
